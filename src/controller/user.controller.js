d5f3a1af4a7d2180bb33a336967aa65cf3098979
Etcetera <yilonglei790@gmail.com>
2022-11-20 12:04:19 +0800

    新建用户链接数据库

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,16 +1,18 @@
 const userService = require('../service/user.service')
-const UserService = require('../service/user.service')
 
 class UserController {
-  create(ctx, next) {
+  async create(ctx, next) {
     // 1.1获取用户传递过来的信息
     const user = ctx.request.body
 
     // 2.将user信息存储到数据库中
-    userService.create(user)
+    const result = await userService.create(user)
 
     // 3.查看存储的结果，告诉前端创建成功
-    ctx.body = '用户创建成功'
+    ctx.body = {
+      message: '用户创建成功',
+      data: result
+    }
   }
 }
 
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,8 +1,16 @@
 const connection = require('../app/database')
 
 class UserService {
-  create(user) {
-    // connection.execute()
+  async create(user) {
+    // 1.获取用户
+    const { name, password } = user
+
+    // 2.拼接statement
+    const statement = 'INSERT INTO `users` (name, password) VALUES (?,?);'
+
+    // 3.执行SQL语句
+    const [result] = await connection.execute(statement, [name, password])
+    return result
   }
 }
 
