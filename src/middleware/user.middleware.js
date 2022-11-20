6d0c987cdcf0616eaa1243070e3e8d80ec76a83c
Etcetera <yilonglei790@gmail.com>
2022-11-20 16:10:44 +0800

    加密密码&存入数据库

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,22 +1,39 @@
+const { 
+  NAME_OR_PASSWORD_IS_REQUIRED,
+  NAME_IS_ALREADY_EXISTS
+} = require('../config/error')
 const userService = require('../service/user.service')
+const md5password = require('../utils/md5-password')
 
 const verifyUser = async (ctx, next) => {
   // 验证客户端传递过来的user是否可以保存到数据库中
   // 1 验证用户名和密码是否为空
   const { name, password } = ctx.request.body
   if (!name || !password) {
-    return ctx.app.emit('error', 'name or password is required', ctx)
+    return ctx.app.emit('error', NAME_OR_PASSWORD_IS_REQUIRED, ctx)
   }
   // 2 判断name是否已经在数据库中存在
   const users = await userService.findUserByName(name)
   if (users.length) {
-    return ctx.app.emit('error', 'name already exists', ctx)
+    return ctx.app.emit('error', NAME_IS_ALREADY_EXISTS, ctx)
   }
 
   // 3.执行下一个中间件
   await next()
 }
 
+const handlePassword = async (ctx, next) => {
+  // 1.去除密码
+  const { password } = ctx.request.body
+
+  // 2.对密码进行加密
+  ctx.request.body.password = md5password(password)
+
+  // 3.执行下一个中间件
+  next()
+}
+
 module.exports = {
-  verifyUser
+  verifyUser,
+  handlePassword
 }
---
+++ src/utils/md5-password.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto')
+
+function md5password(password) {
+  const md5 = crypto.createHash('md5')
+
+  const md5pwd =  md5.update(password).digest('hex')
+
+  return md5pwd
+}
+
+module.exports = md5password
