29381251d7a359c73c7f677da0b0c806b2ff62f6
Etcetera <yilonglei790@gmail.com>
2022-11-22 21:27:15 +0800

    自动化注册

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,13 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
-const userRouter = require('../router/user.router')
-const loginRouter = require('../router/login.router')
+const registerRouters = require('../router')
 
 // 1.创建app
 const app = new Koa()
 
 // 2.对app使用中间件
 app.use(bodyParser())
-app.use(userRouter.routes())
-app.use(userRouter.allowedMethods())
-app.use(loginRouter.routes())
-app.use(loginRouter.allowedMethods())
+registerRouters(app)
 
 // 3.将app导出
 module.exports = app
