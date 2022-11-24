60c3f522c3b342b2ee09ce940de969d589a12e10
Etcetera <yilonglei790@gmail.com>
2022-11-24 12:59:24 +0800

    代码结构

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,5 +1,5 @@
 const KoaRouter = require('@koa/router')
-const userController = require('../controller/user.controller')
+const { create } = require('../controller/user.controller')
 const { verifyUser, handlePassword } = require('../middleware/user.middleware')
 
 // 1.创建路由对象
@@ -7,7 +7,7 @@ const userRouter = new KoaRouter({ prefix: '/users' })
 
 // 2.定义路由中映射
 // 2.1.用户注册接口
-userRouter.post('/', verifyUser, handlePassword , userController.create)
+userRouter.post('/', verifyUser, handlePassword , create)
 
 
 // 3.导出路由
